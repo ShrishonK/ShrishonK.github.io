e9773db0624c0bfa741d9596a051f963aea091dd
ShrishonK <138537786+ShrishonK@users.noreply.github.com>
2024-10-01 22:03:53 -0400

    sample changes to mobile

--- script.js
+++ script.js
@@ -1,61 +1,44 @@
-let text = document.getElementById('text');
+let text = document.getElementById("text");
 
-window.addEventListener('scroll', () => {
-    let value = window.scrollY;
-    text.style.marginTop = value * -2.5 + 'px';
-    header1.style.marginTop = value * -1.2 + 'px';
-    image.style.height = `${value / window.innerHeight} * 200%`;
-    
+window.addEventListener("scroll", () => {
+  let value = window.scrollY;
+  text.style.marginTop = value * -2.5 + "px";
 });
 
-window.addEventListener('scroll', reveal);
+document.getElementById("navbarToggler").addEventListener("click", function () {
+  const navbarCollapse = document.getElementById("navbarCollapse");
+  navbarCollapse.classList.toggle("active");
+});
 
-function reveal(){
-    var reveals = document.querySelectorAll('')
-};
-  
 function updateActiveLink(element) {
-    var navLinks = document.getElementsByClassName("navigation")[0].getElementsByTagName("a");
-    for (var i = 0; i < navLinks.length; i++) {
-        navLinks[i].classList.remove("active");
-        
-    }
-    element.classList.add("active");
+  var navLinks = document
+    .getElementsByClassName("navigation")[0]
+    .getElementsByTagName("a");
+  for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].classList.remove("active");
+  }
+  element.classList.add("active");
 }
 
-// Get all section elements
 const sections = document.querySelectorAll(".title, .section");
 
-// Get nav links 
 const navLinks = document.querySelectorAll(".navigation a");
 
-// Scroll event handler
 window.addEventListener("scroll", () => {
-
-  // Get current scroll position
   let current = "";
-  
-  // Get section in viewport
-  sections.forEach(section => {
-   
-    // Get section ID 
+
+  sections.forEach((section) => {
     const sectionID = section.getAttribute("id");
-    
-    // If scroll position is inside this section
-    if(scrollY >= (section.offsetTop - 50)){
-      current = sectionID; 
+
+    if (scrollY >= section.offsetTop - 50) {
+      current = sectionID;
     }
   });
 
-  // Set active state on link
-  navLinks.forEach(link => {
-   
+  navLinks.forEach((link) => {
     link.classList.remove("active");
-    
-    if(link.getAttribute("href").includes(current)) {
+    if (link.getAttribute("href").includes(current)) {
       link.classList.add("active");
     }
-
   });
-
 });
